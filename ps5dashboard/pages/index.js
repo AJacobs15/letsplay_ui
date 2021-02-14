ae218a025a5981de9dfa8b9df5186d4eb98ea4b4
AJacobs15 <amjacobs19@gmail.com>
2021-02-13 21:07:57 -0500

    nextjs project started

---
+++ ps5dashboard/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+//import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className = "screen">
+      <header>
+        <div className="meadia-nav">
+          
+        </div>
+      </header>
+    </div>
+  )
+}
